1b3367dba77d4ff8f209a01abf3f69c9d8b33955
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2023-11-30 16:48:20 +0700

    HP 15 - 30 Nov 23
    Commit changes from HP 15
    30 November 2023 - 16:48

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,1 +1,22 @@
 import Products from "../models/ProductModel.js";
+
+
+export const getProducts = (req, res) => {
+
+}
+
+export const getProductById = (req, res) => {
+
+}
+
+export const createProduct = (req, res) => {
+
+}
+
+export const updateProduct = (req, res) => {
+
+}
+
+export const deleteProduct = (req, res) => {
+
+}
