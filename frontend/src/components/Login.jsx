3281d70c8a4e7a54fa20dcc424138d485cd25026
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2023-12-16 23:59:41 +0700

    Home - 16 Des 23
    - Added features to Login.jsx
    - Added authSlice.js using react redux toolkit
    - I need to learn redux
    - Added the reducer to store.js
    
    Home - 16 Des 23 - 23:59

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { LoginUser, reset } from "../features/authSlice";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (user || isSuccess) {
+      navigate("/dashboard");
+    }
+    dispatch(reset());
+  }, [user, isSuccess, dispatch, navigate]);
+
+  const Auth = (e) => {
+    e.preventDefault();
+    dispatch(LoginUser({ email, password }));
+  };
+
   return (
     <section className="hero is-fullheight has-background-grey-light is-fullwidth">
       <div className="hero-body">
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-4">
-              <form className="box">
+              <form onSubmit={Auth} className="box">
+                {isError && <p className="has-text-centered">{message}</p>}
                 <h1 className="title is-2">Sign In</h1>
                 <div className="field">
                   <label className="label">Email</label>
                   <div className="control">
                     <input
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                       type="text"
                       className="input"
                       placeholder="example@gmail.com"
@@ -23,15 +49,20 @@ const Login = () => {
                   <label className="label">Password</label>
                   <div className="control">
                     <input
-                      type="text"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="password"
                       className="input"
                       placeholder="********"
                     />
                   </div>
                 </div>
                 <div className="field mt-5">
-                  <button className="button is-success is-fullwidth">
-                    Login
+                  <button
+                    type="submit"
+                    className="button is-success is-fullwidth"
+                  >
+                    {isLoading ? "Loading . . ." : "Login"}
                   </button>
                 </div>
               </form>
