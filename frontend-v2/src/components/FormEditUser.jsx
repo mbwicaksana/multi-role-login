d7d5c01bacc09b27cf4fe6a3746b4b2b1879e260
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2023-12-23 16:49:48 +0700

    Add frontend-v2
    Cek errornya

---
+++ frontend-v2/src/components/FormEditUser.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const FormEditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getUserById = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/users/${id}`);
+        setName(response.data.name);
+        setEmail(response.data.email);
+        setRole(response.data.role);
+      } catch (e) {
+        if (e.response) {
+          setMsg(e.response.data.msg);
+        }
+      }
+    };
+    getUserById();
+  }, [id]);
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:5000/users/${id}`, {
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        role: role,
+      });
+      navigate("/users");
+    } catch (e) {
+      if (e.response) {
+        setMsg(e.response.data.msg);
+      }
+    }
+  };
+  return (
+    <div>
+      <h1 className="title">Users</h1>
+      <h2 className="subtitle">Update User</h2>
+      <div className="card is-shadowless">
+        <div className="card-content">
+          <div className="content">
+            <form onSubmit={updateUser}>
+              <p className="has-text-centered">{msg}</p>
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="example@gmail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Confirm Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="********"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Role </label>
+                <div className="control">
+                  <div className="select is-fullwidth">
+                    <select
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                    >
+                      <option value="admin">Admin</option>
+                      <option value="user">User</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <button type="submit" className="button is-success">
+                    Update
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormEditUser;
