eea1c1398822997f80348d620a7ba140fcfb97c2
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2024-01-05 21:42:07 +0700

    Edit database for deploy to hosting

--- v2/backend/config/Database.js
+++ v2/backend/config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("v2_auth_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  "windelov_v2_auth_db",
+  "windelov_admin",
+  "opz4sz81ydt3",
+  {
+    host: "api.windelov.my.id",
+    dialect: "mysql",
+  }
+);
 
 export default db;
