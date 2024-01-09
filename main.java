3df7223acf24979a015bf7cc4d3464ecfc22eda7
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2024-01-09 16:42:30 +0700

    update

--- main.java
+++ main.java
@@ -1,84 +1,34 @@
-// Import library yang dibutuhkan
-import java.util.Scanner;
-
-// Definisikan class abstrak Shape
-public abstract class Shape {
-
-    // Definisikan atribut
-    protected double p1;
-    protected double p2;
-
-    // Definisikan method abstrak untuk menghitung luas
-    public abstract double getArea();
-
-    // Definisikan constructor
-    public Shape(double p1, double p2) {
-        this.p1 = p1;
-        this.p2 = p2;
+class mains03 {
+    public static void main(String [] aa) {
+        sgtg sg = new sgtg03();
+        prisma03 pr = new prisma03();
+        sg_Jenis();
+        System.out.println(pr.Hitung(4,6));
+        System.out.println(sg.Hitung(6));
+        System.out.println();
+        pr.Jenis();
+        pr.getTinggi(10);
+        System.out.println(sg.Volume());
     }
 }
 
-// Definisikan class persegi yang mengimplementasikan class abstrak Shape
-public class Persegi extends Shape {
-
-    // Definisikan constructor
-    public Persegi(double p1, double p2) {
-        super(p1, p2);
+class sgtg03 {
+    int a = 2;
+    int t = 10;
+    int s = 5;
+    sgtg03(){}
+    sgtg03(int a, int t) {
+        this.a = a;
+        this.t = t;
     }
 
-    // Definisikan method getArea untuk menghitung luas persegi
-    @Override
-    public double getArea() {
-        return p1 * p2;
+    static float Hitung(int a, int t) {
+        return 0.5f*a*t;
     }
-}
-
-// Definisikan class lingkaran yang mengimplementasikan class abstrak Shape
-public class Lingkaran extends Shape {
-
-    // Definisikan constructor
-    public Lingkaran(double p1, double p2) {
-        super(p1, p2);
+    static float Hitung(int s) {
+        s + s + s;
     }
-
-    // Definisikan method getArea untuk menghitung luas lingkaran
-    @Override
-    public double getArea() {
-        return Math.PI * p1 * p1;
-    }
-}
-
-// Definisikan class main
-public class Main {
-
-    // Definisikan variabel scanner
-    static Scanner scanner = new Scanner(System.in);
-
-    // Definisikan method main
-    public static void main(String[] args) {
-
-        // Menerima input dari user
-        System.out.print("Masukkan panjang sisi persegi: ");
-        double p1 = scanner.nextDouble();
-
-        // Menerima input dari user
-        System.out.print("Masukkan lebar sisi persegi: ");
-        double p2 = scanner.nextDouble();
-
-        // Membuat objek persegi
-        Persegi persegi = new Persegi(p1, p2);
-
-        // Menampilkan luas persegi
-        System.out.println("Luas persegi adalah: " + persegi.getArea());
-
-        // Menerima input dari user
-        System.out.print("Masukkan jari-jari lingkaran: ");
-        double r = scanner.nextDouble();
-
-        // Membuat objek lingkaran
-        Lingkaran lingkaran = new Lingkaran(r, r);
-
-        // Menampilkan luas lingkaran
-        System.out.println("Luas lingkaran adalah: " + lingkaran.getArea());
+    public void Jenis() {
+        System.out.println("Segitiga sama kaki")
     }
 }
